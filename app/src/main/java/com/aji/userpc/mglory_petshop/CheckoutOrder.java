e69b8200488175b84c4e216ee23560ad94abce89
Adi Purnomo <adipurnomosidik97@gmail.com>
2018-04-09 15:39:12 +0700

    Fixing Login with facebook dan Total pembayaran

--- app/src/main/java/com/aji/userpc/mglory_petshop/CheckoutOrder.java
+++ app/src/main/java/com/aji/userpc/mglory_petshop/CheckoutOrder.java
@@ -3,11 +3,15 @@ package com.aji.userpc.mglory_petshop;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class CheckoutOrder extends AppCompatActivity {
+/**
+ * Created by Adi Purnomo Sidik on 4/9/2018.
+ */
 
+public class CheckoutOrder extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkout_order);
     }
 }
+
