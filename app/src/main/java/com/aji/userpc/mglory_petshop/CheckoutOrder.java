34e4aac1264bf7f21f7933b65c51fab073fe5bc6
Adi Purnomo <adipurnomosidik97@gmail.com>
2018-04-23 11:55:21 +0700

    Fungsi Checkout Order

--- app/src/main/java/com/aji/userpc/mglory_petshop/CheckoutOrder.java
+++ app/src/main/java/com/aji/userpc/mglory_petshop/CheckoutOrder.java
@@ -1,17 +1,21 @@
 package com.aji.userpc.mglory_petshop;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-/**
- * Created by Adi Purnomo Sidik on 4/9/2018.
- */
+import android.widget.TextView;
 
 public class CheckoutOrder extends AppCompatActivity {
+    String totalharga;
+    TextView tvTotal;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkout_order);
+        Intent i = getIntent();
+        totalharga = i.getStringExtra("total");
+        tvTotal = findViewById(R.id.tvTotalHarga);
+
+        tvTotal.setText(totalharga);
     }
 }
-
